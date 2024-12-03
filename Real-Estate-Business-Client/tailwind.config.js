9dd75040d94712328081d18d17e4ae3cc1117e2b
S0k1l <martynuky16@gmail.com>
2024-12-03 21:36:22 +0200

    Added Navbar

--- Real-Estate-Business-Client/tailwind.config.js
+++ Real-Estate-Business-Client/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          8: "#141414",
+          10: "#1A1A1A",
+          15: "#262626",
+          20: "#333333",
+          30: "#4D4D4D",
+          40: "#666666",
+          "50-2": "#808080",
+          60: "#999999",
+        },
+      },
+      boxShadow: {
+        "inset-1-gray-custom": "inset 0px 0px 0px 1px #262626",
+      },
+    },
   },
   plugins: [],
 };
