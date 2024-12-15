b1b0d32a261d1bf3175c7d883d97c7954e5c72ae
S0k1l <martynuky16@gmail.com>
2024-12-15 22:07:48 +0200

    Added CTA section

--- Real-Estate-Business-Client/src/app/app.component.ts
+++ Real-Estate-Business-Client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CtaComponent } from './components/cta/cta.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CtaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
