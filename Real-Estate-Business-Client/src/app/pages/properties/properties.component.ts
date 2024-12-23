ec256924c05d7efcc14dfd9d0014c346d397dbf6
S0k1l <martynuky16@gmail.com>
2024-12-23 18:09:26 +0200

    Added Search & Filters section (Properties page)

--- Real-Estate-Business-Client/src/app/pages/properties/properties.component.ts
+++ Real-Estate-Business-Client/src/app/pages/properties/properties.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DividerComponent } from '../../components/divider/divider.component';
 
 @Component({
   selector: 'app-properties',
   standalone: true,
-  imports: [],
+  imports: [DividerComponent],
   templateUrl: './properties.component.html',
   styleUrl: './properties.component.css',
 })
