2194343d8eb6762cd2b9a3148fd7c25aca8f061a
S0k1l <martynuky16@gmail.com>
2024-12-23 18:08:46 +0200

    Added Header Section section (Properties page)

--- Real-Estate-Business-Client/src/app/app.routes.ts
+++ Real-Estate-Business-Client/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: 'properties',
+    component: PropertiesComponent,
   },
 ];
