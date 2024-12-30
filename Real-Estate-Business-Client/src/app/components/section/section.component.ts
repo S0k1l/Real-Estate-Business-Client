41c41a912ff69fbb3d2ec31ad37d97afc0ea0390
S0k1l <martynuky16@gmail.com>
2024-12-30 18:47:57 +0200

    Code cleaning

--- Real-Estate-Business-Client/src/app/components/section/section.component.ts
+++ Real-Estate-Business-Client/src/app/components/section/section.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { StarsDividerComponent } from '../stars-divider/stars-divider.component';
 import { CommonModule } from '@angular/common';
 
@@ -9,11 +9,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './section.component.html',
   styleUrl: './section.component.css',
 })
-export class SectionComponent implements OnInit {
-  ngOnInit(): void {
-    console.log('isButton: ' + this.isButton);
-    console.log('isRightSection: ' + this.isRightSection);
-  }
+export class SectionComponent {
   @Input() header: string = 'no text';
   @Input() text: string = 'no text';
   @Input() isButton: boolean = false;
