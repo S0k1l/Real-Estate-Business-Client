dc17200473d9bc821c7c5ddd338c90217a6c3369
S0k1l <martynuky16@gmail.com>
2025-01-03 16:54:19 +0200

    Added Contact Form section (Contact page)

--- Real-Estate-Business-Client/src/app/pages/contact/contact.component.ts
+++ Real-Estate-Business-Client/src/app/pages/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ServiceItemBtnComponent } from '../../components/service-item-btn/service-item-btn.component';
+import { SectionComponent } from '../../components/section/section.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [ServiceItemBtnComponent],
+  imports: [ServiceItemBtnComponent, SectionComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
