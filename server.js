c51c4faf8a4041285e7a78a877fce512c85df226
S0k1l <martynuky16@gmail.com>
2025-01-18 21:24:35 +0200

    Prepare to deploy

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Serve static files from the Angular dist directory
+app.use(
+  express.static(
+    path.join(__dirname, "dist/real-estate-business-client/browser")
+  )
+);
+
+// Redirect all requests to the index.html
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "dist/real-estate-business-client/browser/index.html")
+  );
+});
+
+// Start the app by listening on the default Render port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
